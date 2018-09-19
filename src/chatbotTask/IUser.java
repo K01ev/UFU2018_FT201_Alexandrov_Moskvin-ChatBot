eb9df644825c699f2184b1a079de6ad5545326aa
Илья <you@example.com>
2018-09-19 22:34:59 +0500

    Добавил интерфейс генератора вопросов и 1 его реализацию. Считаю ненужным на данном этапе класс User, подробнее см. в классе ChatBot. Можешь отталкиваться от этой версии, можешь отталкиваться от своей, но нужно обсуждение.

--- src/chatbotTask/IUser.java
+++
@@ -1,7 +0,0 @@
-package chatbotTask;
-
-public interface IUser {
-	
-	
-	
-}
--- src/chatbotTask/Main.java
+++ src/chatbotTask/Main.java
@@ -5,22 +5,18 @@ public class Main {
 
 	//String path = System.getProperties("user.dir");
 	public static void main(String[] args) {	
-		IChatBot bot = new ChatBot();
+		ChatBot bot = new ChatBot(new QuestionsRepository());
 		IMessageProvider provider = new ConsoleMessageProvider();
 		provider.sendMessage(bot.getHelp());
 		while(true) {
-			Pair<Integer, String> questionPair = bot.getNextQuestion();
-			int id = questionPair.getFirst();
-			provider.sendMessage(questionPair.getSecond());
+			String question = bot.getNextQuestion();
+			provider.sendMessage(question);
 			String message = provider.getMessage();
 			
-			if (message.equals("/help")) 
-				provider.sendMessage(bot.getHelp());
-			else if (message.equals("/exit"))
+			if (message.equals("/exit"))
 				break;
-			else 
-				provider.sendMessage(bot.checkAnswer(id, message) ? 
-						"Correct!" : "Incorrect!");
+			
+			provider.sendMessage(bot.checkAnswer(message));
 		}
 	}
 }	
